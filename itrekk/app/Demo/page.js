141c5812b5501509bfee26f374399cf31464ca1b
Michael Y Chuang <85854593+mchuangyc413@users.noreply.github.com>
2024-03-27 22:25:02 -0700

    imgs

--- itrekk/app/Demo/page.js
+++ itrekk/app/Demo/page.js
@@ -3,18 +3,16 @@ import React from "react";
 import Navbar from "../components/Navbar";
 
 const Page = () => {
-  // Array of image names
-  const imageNames = ['1', '2', '3', '4', '5'];
-
   return (
     <>
       <Navbar />
       <div className="flex justify-center items-center h-screen">
         <div className="rounded-lg shadow-xl bg-base-100 max-h-[80vh] w-11/12 md:w-3/4 lg:w-2/3 overflow-auto p-8">
-          {/* Map through the imageNames array to display images */}
-          {imageNames.map((imageName, index) => (
-            <img src="/assets/${imageName}.png" className="h-8  mb-2 mr-1" />
-          ))}
+          <img src="/assets/img1.png" alt="Image 1" className="h-8 mb-2 mr-1" />
+          <img src="/assets/2.png" alt="Image 2" className="h-8 mb-2 mr-1" />
+          <img src="/assets/3.png" alt="Image 3" className="h-8 mb-2 mr-1" />
+          <img src="/assets/4.png" alt="Image 4" className="h-8 mb-2 mr-1" />
+          <img src="/assets/5.png" alt="Image 5" className="h-8 mb-2 mr-1" />
         </div>
       </div>
     </>
